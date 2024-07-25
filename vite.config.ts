a5a11e6fb24d98c0ae295d482ec4f881a45c002d
Donald APOVO <apovodonald@gmail.com>
2024-07-25 18:20:43 +0100

    add github io deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/form-repeater/" : "/",
 });
